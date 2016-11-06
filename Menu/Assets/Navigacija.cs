6a5a848d9927a933ae97f5c1881f382d2c37d5bd
ProfesorDule <dusanz@bobaz.net>
2016-11-06 12:17:42 +0100

    Dodavanje prvo

---
+++ Menu/Assets/Navigacija.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Navigacija : MonoBehaviour {
+
+	public EventSystem eventSystem;
+	public GameObject izabraniGameObject;
+
+	private bool izabranoDugme;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetAxisRaw ("Vertical") != 0 && izabranoDugme == false)
+		{
+			eventSystem.SetSelectedGameObject(izabraniGameObject);
+			izabranoDugme = true;
+		}
+	
+	}
+	
+	private void OnDisable()
+	{
+		izabranoDugme = false;
+	}
+}
---
+++ Menu/Assets/Promena.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Promena : MonoBehaviour {
+
+	public void IzaberiScenu(int Index)
+	{
+		SceneManager.LoadScene(Index);
+	}
+}
